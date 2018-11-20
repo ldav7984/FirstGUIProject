189237f4acbebd331e121137c9ce41504a5da8bc
Davis <ldav7984@740s-pl-109.local>
2018-11-20 12:22:26 -0700

    Added a method to set up the frame.

--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -8,7 +8,15 @@ public class FirstFrame extends JFrame //it is a customized JFrame
 	{
 		super(); //the class inherits from another class so it MUST start with super();
 		
-		
+	}
+	
+	
+	private void setupFrame()
+	{
+		this.setSize(800, 900);
+		this.setTitle("My first GUI");;
+		this.setResizable(true);
+		this.setVisible(true);;
 	}
 	
 }
