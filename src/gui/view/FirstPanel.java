1b402fb63c247be7b981b725a0c9a579cceb1e8c
Davis <ldav7984@740s-pl-109.local>
2018-11-20 12:36:52 -0700

    Added JPanel class.

---
+++ src/gui/view/FirstPanel.java
@@ -0,0 +1,31 @@
+package gui.view;
+
+import javax.swing.JPanel;
+
+public class FirstPanel extends JPanel
+{
+	public FirstPanel()
+	{
+		super();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+}
