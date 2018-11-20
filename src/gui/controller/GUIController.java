be27a9372bef3702f9a085806438026c984cef14
Davis <ldav7984@740s-pl-109.local>
2018-11-20 12:13:26 -0700

    Initial commit.

---
+++ src/gui/controller/GUIController.java
@@ -0,0 +1,16 @@
+package gui.controller;
+
+public class GUIController 
+{
+
+	public GUIController() //constructor
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+	}
+	
+}
---
+++ src/gui/controller/GUIRunner.java
@@ -0,0 +1,12 @@
+package gui.controller;
+
+public class GUIRunner 
+{
+	public static void mani(String [ ] args)
+	{
+		GUIController app = new GUIController();
+		app.start();
+	}
+	
+	
+}
